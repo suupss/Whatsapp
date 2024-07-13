3db67b1f3b1b2f28af63436bd8678a48248bf3fb
SupriyaAdhikari <demiandsupriya@gmail.com>
2024-07-13 07:43:40 +0545

    feat: add taskbar view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:work_app/chat_page.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
