56fc7f9cefb87c1a98070cf6dfb871a8c6764756
SupriyaAdhikari <demiandsupriya@gmail.com>
2024-07-18 14:11:36 +0545

    style: remove whitespace

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:work_app/home_page.dart';
-import 'package:work_app/login_page.dart';
-import 'package:work_app/sign_up_page.dart';
+import 'package:work_app/home_page/view/home_page_view.dart';
+import 'package:work_app/login_page/view/login_page_view.dart';
+import 'package:work_app/signup_page/view/signup_page_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,14 +24,14 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 173, 32, 166),
           foregroundColor: Colors.white, //here you can give the text color
         ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: const Color.fromARGB(255, 173, 32, 166),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Color.fromARGB(255, 173, 32, 166),
             foregroundColor: Colors.white),
         elevatedButtonTheme: ElevatedButtonThemeData(
             style: TextButton.styleFrom(
                 backgroundColor: const Color.fromARGB(255, 173, 32, 166),
                 foregroundColor: Colors.white)),
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           headlineMedium: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
           headlineSmall: TextStyle(fontSize: 15, height: 2),
         ),
